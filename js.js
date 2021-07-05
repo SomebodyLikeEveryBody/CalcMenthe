46a5d4230e9277bb42ce744480ac0b5acd81c794
r0b3rt[0] <nullepart@rien.com>
2021-07-05 07:36:07 +0200

    need to implement the proposeQuestion function in QuestionWidget and not in the controller

--- js.js
+++ js.js
@@ -1,13 +1,39 @@
+function rand(pMin, pMax, pInteger) {
+	if(pInteger)
+		return Math.floor(((Math.random() * (pMax-pMin+1)) + pMin));		
+	else
+    		return ((Math.random() * (pMax-pMin)) + pMin);
+}
+
 function Controller(pQuestionWidget, pAnswerWidget) {
+    this.QUESTION_ASKED_STATUS = 0;
+    this.QUESTION_ANSWERED_STATUS = 1;
+    this.UNDEFINED_STATUS = -1;
+
     this.questionWidget = pQuestionWidget;
     this.answerWidget = pAnswerWidget;
+    this.question = '';
+    this.status = this.UNDEFINED_STATUS;
+
+    this.proposeQuestion = function () {
+        this.question = rand(1, 50, true) + '^2';
+        this.questionWidget.setContent(this.question + ' = ?');
+        this.status = this.QUESTION_ASKED_STATUS;
+    }
 
     this.setEvents = function () {
         let that = this;
 
         this.questionWidget.jqEl.click(function () {
-            that.answerWidget.clear();
-            that.answerWidget.focus();
+            // if (that.status === QUESTION_ANSWERED_STATUS) {
+                that.questionWidget.hide();
+                that.proposeQuestion();
+                that.questionWidget.show();
+                that.answerWidget.clear();
+                that.answerWidget.focus();
+            // } else if (that.status === QUESTION_ASKED_STATUS) {
+
+            // }
         });
 
         this.answerWidget.jqEl.keydown(function (e) {
@@ -15,17 +41,30 @@ function Controller(pQuestionWidget, pAnswerWidget) {
                 that.questionWidget.click();
             }
         });
-    }
+    };
 
     this.init = function () {
         this.setEvents();
-    }
+        this.proposeQuestion();
+    };
 }
 
 function QuestionWidget() {
     this.jqEl = $('#question');
+    this.contentEl = this.jqEl.find('span');
+
+    this.convertToLatex = function (pText) {
+        return M(pText, true);
+    }
 
     this.click = () => this.jqEl.click();
+    this.hide = () => this.contentEl.hide(500);
+    this.show = () => this.contentEl.fadeIn(500);
+    this.clearContent = () => this.contentEl.html('');
+    this.setContent = function (ptextContent) {
+        this.clearContent();
+        this.contentEl.append(this.convertToLatex(ptextContent));
+    };
 
     this.init = function () {
 
